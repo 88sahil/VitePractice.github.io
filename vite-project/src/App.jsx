81ef0249a25ba99af8e59fc3417451fcab4f68e7
88sahil <sahilmaliya59@gmail.com>
2023-10-15 15:35:48 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,14 +3,20 @@ import { useState } from 'react'
 import './App.css'
 
 export const App = () => {
-  let [counter,setcounter] = useState(15)
+  let [counter,setcounter] = useState(1)
+  let clock = true
   function addvalue(){
-    counter = counter+1
-    setcounter(counter)
+    if(clock==true)
+      {
+       setInterval.current =  setInterval(()=>{
+        counter = counter+1
+        setcounter(counter)
+      },1000)
+    }
   }
+
   function removeValue(){
-    counter = counter-1
-    setcounter(counter)
+
   }
   return (
     <div>
