7fc2c718be69a0dd78d4cce47bb9b9094fab9110
88sahil <sahilmaliya59@gmail.com>
2023-10-15 11:25:38 +0530

    nan

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
 export const App = () => {
+  let [counter,setcounter] = useState(15)
+  function addvalue(){
+    counter = counter+1
+    setcounter(counter)
+  }
+  function removeValue(){
+    counter = counter-1
+    setcounter(counter)
+  }
   return (
-    <div>App</div>
+    <div>
+      <h1>counter is : {counter}</h1>
+      <button onClick={addvalue}>Add value</button>
+      <button onClick={removeValue}>Remove value</button>
+    </div>
   )
 }
+
+export default App
