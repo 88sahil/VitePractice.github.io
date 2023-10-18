d6056a534abad6a2dc280f9b6116ebc68645b085
88sahil <sahilmaliya59@gmail.com>
2023-10-18 19:14:39 +0530

    new

---
+++ react-router/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link,NavLink } from 'react-router-dom'
+const Header = () => {
+  return (
+    <header className='shadow sticky z-50 top-0'>
+        
+    </header>
+  )
+}
+
+export default Header
