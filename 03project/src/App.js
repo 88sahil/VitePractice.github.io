8f1ee84e062fa2119aea0bcd7193a7090f9be232
88sahil <sahilmaliya59@gmail.com>
2023-10-18 15:49:37 +0530

    new

---
+++ 03project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='div1  flex justify-center  w-full bg-gray-500 h-60 text-center'>
+      <div className='div2 h-28 w-28 bg-black '>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
