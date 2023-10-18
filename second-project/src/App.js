92198888098849a87772de15162d4de6f3ca61b3
88sahil <sahilmaliya59@gmail.com>
2023-10-18 12:17:01 +0530

    complete project

--- second-project/src/App.js
+++ second-project/src/App.js
@@ -1,17 +1,58 @@
 import React from 'react'
+import {useState,useCallback,useEffect,useRef} from 'react'
 import './App.css'
-import Person from './components/Person'
-import Card from './components/Card'
+
 
 const App = () => {
+  let textare = document.getElementById("passwordarea")
+const [length,setlength] = useState(8)
+const [numbersAl,setnumberAl] = useState(false)
+const [charAl,setcharAl] = useState(false)
+const [pass,setpass]=useState("")
+let passref = useRef(null)
+const passwordgenerator = useCallback(()=>{
+  let pass=""
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if(numbersAl) str+="0123456789";
+  if(charAl) str+="`~!@#$[{]}%^&*()_+=,<.>/?";
+
+  for(let i=1;i<=length;i++){
+    let char = Math.floor(Math.random()* str.length)
+
+    pass += str.charAt(char)
+  }
+  setpass(pass)
+},[length,numbersAl,charAl,setpass])
+
+useEffect(()=>{
+  passwordgenerator()
+},[length,charAl,numbersAl,passwordgenerator])
+
+const Copy = useCallback(()=>{
+  passref.current?.select()
+  window.navigator.clipboard.writeText(pass)
+},[pass])
   return (
-    <div className='flex flex-wrap'>
-      {Person.map(i=>(
-        <Card
-          background={i.background}
-        />
-))}
-</div>
+   
+   <div className='w-full  min-w-md bg-black '>
+    <div className='div2 bg-gray-500 w-full '>
+      <h1 className='nan text-white text-center text-3xl p-5'>passwordgenerator</h1>
+      <div className='flex justify-center pb-11'>
+        <input type='text' className='textbox pl-2 py-4 shadow-md ' placeholder='password' id="passwordarea" value={pass} readOnly ref={passref}></input>
+        <button className='py-4 bg-blue-600 px-8 text-teal-100 shadow-md' onClick={Copy}>Copy</button>
+      </div>
+      <div className='flex justify-center pb-4'>
+        <input type="range" min={8} max={100} onChange={(e)=>{setlength(e.target.value)}}></input>
+        <label className='nan text-orange-500 '>length:{length}</label>
+        <input type="checkbox" defaultChecked={numbersAl} className='ml-2' onChange={()=>{setnumberAl((prev) => !prev)}}></input>
+        <label className='ml-1'>Number</label>
+        <input type="checkbox" defaultChecked={charAl} className='ml-2' onChange={()=>{setcharAl((prev)=> !prev)}}></input>
+        <label className='ml-1'>Characters</label>
+      </div>
+    </div>
+
+   </div>
+
   )
 }
 
