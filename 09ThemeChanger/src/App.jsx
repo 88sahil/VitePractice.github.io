1b168bb9046376fa9a929500ceb289dcc0387c14
88sahil <sahilmaliya59@gmail.com>
2023-10-20 22:38:20 +0530

    new

--- 09ThemeChanger/src/App.jsx
+++ 09ThemeChanger/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { ThemeContextProvider } from './context/ThemeContext'
 import ThmemeButton from './components/ThmemeButton'
+import Card from './components/Card'
 const App = () => {
   const [thememode,setthememode] = useState("light")
   const darkmode =()=>{
@@ -10,18 +11,18 @@ const App = () => {
     setthememode("light")
   }
   useEffect(()=>{
-    document.querySelector("html").remove("dark","light")
-    document.querySelector("html").addEventListener(thememode)
+    document.querySelector("html").classList.remove("dark","light")
+    document.querySelector("html").classList.add(thememode)
   },[thememode])
   return (
-    <ThemeContextProvider values={{thememode,darkmode,lightmode}}>
-      <div className='flex flex-wrap min-h-screen justify-center'>
+    <ThemeContextProvider value={{thememode,darkmode,lightmode}}>
+      <div className='flex flex-wrap min-h-screen justify-center items-center'>
         <div className='w-full'>
           <div className='flex w-full max-w-sm mx-auto justify-end mb-4'>
             <ThmemeButton/>
           </div>
           <div className='w-full max-w-sm mx-auto'>
-            {/* card */}
+            <Card/>
           </div>
         </div>
 
